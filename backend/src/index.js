60d2c2c7f1a6c20739a9862a60b204e33ac5549e
Eden Park <edenGoforit@gmail.com>
2021-06-15 21:35:07 +1200

    Feat:nodemon installed and set in as scripts in package.json

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+const port = 4000;
+
+app.use((ctx) => {
+  ctx.body = 'hello world';
+});
+
+app.listen(port, () => {
+  console.log(`Listening to the port ${port}`);
+});
