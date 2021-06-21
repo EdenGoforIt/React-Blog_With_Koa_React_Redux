0874e955bb0eac12e887dd5160ed1909e19b17b1
Eden Park <edenGoforit@gmail.com>
2021-06-21 20:51:52 +1200

    Feat: connect postcontroller into barren index.js

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -1,18 +1,13 @@
 const Router = require('koa-router');
+const postController = require('./posts.ctrl');
+
 const posts = new Router();
 
-const printInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+posts.get('/', postController.list);
+posts.post('/', postController.write);
+posts.get('/:id', postController.read);
+posts.delete('/:id', postController.remove);
+posts.put('/:id', postController.replace);
+posts.patch('/:id', postController.update);
 
 module.exports = posts;
