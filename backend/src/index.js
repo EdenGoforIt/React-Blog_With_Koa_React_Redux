b3c6229ff768901d6f6c2d60deb3e92dceb6e1fa
Eden Park <edenGoforit@gmail.com>
2021-06-22 21:39:07 +1200

    Feat: mongo db connected from  index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,26 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 const api = require('./api');
 const app = new Koa();
 const router = new Router();
 
 // eslint-disable-next-line no-undef
-const { PORT } = process.env;
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to Mongo');
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 router.use('/api', api.routes());
 
