31881635e4e2387eaae8431ccbc79147ad0c8181
Eden Park <edenGoforit@gmail.com>
2021-06-22 21:23:03 +1200

    Feat: .env created and added environmental variables

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const api = require('./api');
 const app = new Koa();
 const router = new Router();
-const port = 4000;
+
+// eslint-disable-next-line no-undef
+const { PORT } = process.env;
 
 router.use('/api', api.routes());
 
@@ -13,6 +16,7 @@ router.use(bodyParser());
 
 app.use(router.routes()).use(router.allowedMethods());
 
+const port = PORT || 4000;
 app.listen(port, () => {
   console.log(`Listening to the port ${port}`);
 });
