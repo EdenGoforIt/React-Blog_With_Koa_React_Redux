ca7e0b6d77eb3d0e691db4c209235e7e298ffdc3
Eden Park <edenGoforit@gmail.com>
2021-07-01 22:03:08 +1200

    Feat: added Joi for validating requesting body, added validation to the post create method

--- backend/src/api/posts/posts.ctrl.js
+++ backend/src/api/posts/posts.ctrl.js
@@ -1,5 +1,6 @@
 import Post from '../../models/post';
 import mongoose from 'mongoose';
+import Joi from 'joi';
 
 const { ObjectId } = mongoose.Types;
 
@@ -13,6 +14,20 @@ export const checkObjectId = (ctx, next) => {
 };
 
 export const write = async (ctx) => {
+  //validation
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
   const { title, body, tags } = ctx.request.body;
   const post = new Post({
     title,
