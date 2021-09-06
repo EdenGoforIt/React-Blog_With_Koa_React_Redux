95d016f0f711be3650f1781dc7ffc3c58997f7f8
Eden Park <edenGoforit@gmail.com>
2021-09-06 21:57:39 +1200

    Feat:Register From has been added

---
+++ frontend/src/components/auth/RegisterForm.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
+
+const RegisterForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({
+    form: auth.register,
+  }));
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(changeField({ form: 'register', key: name, value }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    dispatch(initializeForm('register'));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="register"
+      form={form}
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default RegisterForm;
