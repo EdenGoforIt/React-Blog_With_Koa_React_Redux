88253bace8045b55ced54d73a46a77f9bb753a99
PS\EdenP <EdenP@provoke.co.nz>
2021-09-22 21:33:07 +1200

    Feat: CreateSagaRequest created

--- frontend/src/modules/auth.js
+++ frontend/src/modules/auth.js
@@ -4,6 +4,15 @@ import produce from 'immer';
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
+const REGISTER = 'auth/REGISTER';
+const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+const LOGIN = 'auth/LOGIN';
+const LOGIN_SUCCESS = 'auth/LOGIN/SUCCESS';
+const LOGIN_FAILURE = 'AUTH/LOGIN/FAILURE';
+
+
 const initialState = {
   register: {
     username: '',
