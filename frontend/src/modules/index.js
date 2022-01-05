9c83acf50ea2521c0e16b16ff1928af635310be7
EdenPark <eden.park@provoke.co.nz>
2022-01-06 11:47:38 +1300

    Feat: added auth reducer to the root reducer

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,14 +1,16 @@
 import { combineReducers } from 'redux';
 import auth, { authSaga } from './auth';
+import user, { userSaga } from './user';
 import loading from './loading';
 import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
   auth,
+  user,
   loading,
 });
 export function* rootSaga() {
-  yield all(authSaga());
+  yield all(authSaga(), userSaga());
 }
 
 export default rootReducer;
