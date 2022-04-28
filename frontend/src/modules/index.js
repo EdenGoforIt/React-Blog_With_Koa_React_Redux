3204aeaf7c5ac4bf16cd3cc92f12dc2f71fd5f8c
EdenPark <edengoforit@gmail.com>
2022-04-28 20:47:11 +1200

    ADded post reducer in the barrel file

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -4,16 +4,18 @@ import auth, { authSaga } from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
 import write, { writeSaga } from './write';
+import post, { postSaga } from './post';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
   write,
+  post,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga(), writeSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
 }
 
 export default rootReducer;
