b5bb86962be5c058bff0029043fb11bb0094db58
EdenPark <edengoforit@gmail.com>
2022-06-24 12:06:22 +1200

    added more endpoints in post list

--- frontend/src/lib/api/posts.js
+++ frontend/src/lib/api/posts.js
@@ -1,6 +1,28 @@
+import qs from 'qs';
 import client from './client';
 
 export const writePost = ({ title, body, tags }) =>
   client.post('/api/posts', { title, body, tags });
 
 export const readPost = (id) => client.get(`/api/posts/${id}`);
+
+export const listPosts = ({ page, username, tag }) => {
+  console.log('here');
+  const queryString = qs.stringify({
+    page,
+    username,
+    tag,
+  });
+  return client.get(`/api/posts?${queryString}`);
+};
+export const listPosts2 = ( ) => { 
+  return client.get(`/api/posts`);
+};
+export const updatePost = ({ id, title, body, tags }) =>
+  client.patch(`/api/posts/${id}`, {
+    title,
+    body,
+    tags,
+  });
+
+export const removePost = (id) => client.delete(`/api/posts/${id}`);
