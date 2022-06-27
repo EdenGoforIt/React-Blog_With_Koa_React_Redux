cefab93d405701cb130da1b47a66daeb86604aea
EdenPark <edengoforit@gmail.com>
2022-06-27 20:47:28 +1200

    post list container added

---
+++ frontend/src/containers/post/PostListContainer.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import qs from 'qs';
+import { withRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import PostList from '../../components/posts/PostList';
+import { listPosts, listPosts2 } from '../../modules/posts';
+
+const PostListContainer = ({ location, match }) => {
+  const dispatch = useDispatch();
+  const { posts, error, loading, user } = useSelector(
+    ({ posts, loading, user }) => ({
+      posts: posts?.posts,
+      error: posts?.error,
+      loading: loading['posts/LIST_POSTS'],
+      user: user.user,
+    }),
+  );
+  useEffect(() => {
+    const { username } = match.params;
+    const { tag, page } = qs.parse(location.search, {
+      ignoreQueryPrefix: true,
+    });
+    dispatch(listPosts({ tag, username, page }));
+  }, [dispatch, location.search, match.params]);
+
+  return (
+    <PostList
+      loading={loading}
+      error={error}
+      posts={posts}
+      showWriteButton={user}
+    />
+  );
+};
+
+export default withRouter(PostListContainer);
