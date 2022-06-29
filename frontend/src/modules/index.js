607f37838d2812f091685233206185409b9b3336
EdenPark <edengoforit@gmail.com>
2022-06-29 20:52:59 +1200

    added posts to the root reducer

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -5,6 +5,7 @@ import loading from './loading';
 import user, { userSaga } from './user';
 import write, { writeSaga } from './write';
 import post, { postSaga } from './post';
+import posts, { postsSaga } from './posts';
 
 const rootReducer = combineReducers({
   auth,
@@ -12,10 +13,11 @@ const rootReducer = combineReducers({
   user,
   write,
   post,
+  posts
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga(), postSaga(), postsSaga()]);
 }
 
 export default rootReducer;
