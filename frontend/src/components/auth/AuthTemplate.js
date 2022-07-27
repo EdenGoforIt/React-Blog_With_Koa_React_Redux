31a1c4089d386e5a39e4fb68eb6531c55d87ed8a
EdenPark <edengoforit@gmail.com>
2022-07-27 20:29:50 +1200

    Change Auth logo link

--- frontend/src/components/auth/AuthTemplate.js
+++ frontend/src/components/auth/AuthTemplate.js
@@ -43,7 +43,7 @@ const AuthTemplate = ({ children }) => {
     <AuthTemplateBlock>
       <WhiteBox>
         <div className="logo-area">
-          <Link to="/">REACTERS</Link>
+          <Link to="/">Eden</Link>
         </div>
         {children}
       </WhiteBox>
