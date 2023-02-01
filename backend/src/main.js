a88cb4575b95d9d64d88dc9c44f04bc79750efa8
EdenPark <edengoforit@gmail.com>
2023-02-02 07:53:54 +1300

    react version upgraded

--- backend/src/main.js
+++ backend/src/main.js
@@ -10,11 +10,14 @@ import jwtMiddleware from './lib/jwtMiddleware';
 const { PORT, MONGO_URI } = process.env;
 
 mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true })
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('Connected to MongoDB');
   })
-  .catch(e => {
+  .catch((e) => {
     console.error(e);
   });
 
