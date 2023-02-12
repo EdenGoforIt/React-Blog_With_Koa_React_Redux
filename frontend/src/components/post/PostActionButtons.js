31d5a9fdcbeeafc2dadc2fd397b725b65d12b157
EdenPark <edengoforit@gmail.com>
2023-02-12 19:11:15 +1300

    post aciton buttons added

---
+++ frontend/src/components/post/PostActionButtons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../styles/palette';
+
+const PostActionButtonBlock = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 2rem;
+  margin-top: -1.5rem;
+`;
+
+const ActionButton = styled.button`
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  color: ${palette.gray[1]};
+  font-weight: bold;
+  border: none;
+  outline: none;
+  font-size: 0.875rem;
+  cursor: pointer;
+  &:hover {
+    background: ${palette.gray[1]};
+    color: ${palette.cyan[7]};
+  }
+  & + & {
+    margin-left: 0.25rem;
+  }
+`;
+
+const PostActionButtons = () => {
+  return (
+    <PostActionButtonBlock>
+      <ActionButton>Edit</ActionButton>
+      <ActionButton>Delete</ActionButton>
+    </PostActionButtonBlock>
+  );
+};
