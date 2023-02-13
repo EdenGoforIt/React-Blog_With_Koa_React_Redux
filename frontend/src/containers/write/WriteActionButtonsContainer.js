272f7eb175ccdbc7b2f9e1a023dfe6897e8aaf5a
EdenPark <edengoforit@gmail.com>
2023-02-14 08:08:21 +1300

    write actionbutton container for edit

--- frontend/src/containers/write/WriteActionButtonsContainer.js
+++ frontend/src/containers/write/WriteActionButtonsContainer.js
@@ -2,20 +2,28 @@ import React, { useEffect } from 'react';
 import WriteActionButtons from '../../components/write/WriteActionButtons';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { writePost } from '../../modules/write';
+import { writePost, updatePost } from '../../modules/write';
 
 const WriteActionButtonsContainer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
-    title: write.title,
-    body: write.body,
-    tags: write.tags,
-    post: write.post,
-    postError: write.postError,
-  }));
+  const { title, body, tags, post, postError, originalPostId } = useSelector(
+    ({ write }) => ({
+      title: write.title,
+      body: write.body,
+      tags: write.tags,
+      post: write.post,
+      postError: write.postError,
+      originalPostId: write.originalPostId,
+    }),
+  );
 
   const onPublish = () => {
+    if (originalPostId) {
+      dispatch(updatePost({ title, body, tags, id: originalPostId }));
+      return;
+    }
+
     dispatch(
       writePost({
         title,
@@ -40,7 +48,13 @@ const WriteActionButtonsContainer = () => {
       console.log(postError);
     }
   }, [navigate, post, postError]);
-  return <WriteActionButtons onPublish={onPublish} onCancel={onCancel} />;
+  return (
+    <WriteActionButtons
+      onPublish={onPublish}
+      onCancel={onCancel}
+      isEdit={!!originalPostId}
+    />
+  );
 };
 
 export default WriteActionButtonsContainer;
